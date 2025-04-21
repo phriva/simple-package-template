f1e4b3675527f0e01f78b2f03419b2600f079015
phriva <119083486+phriva@users.noreply.github.com>
2025-04-21 20:24:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="helloworld",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Peter",
+    author_email="riva.pedrohenrique@gmail.com",
+    description="This is a basic Hello World",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="github.com/phriva/"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
